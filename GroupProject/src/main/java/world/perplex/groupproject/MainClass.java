ace171771c825d5e64c3b21ed85a498f682c35d9
Rolf Bänziger <rolf@perplex.world>
2022-03-17 15:52:48 +0000

    Added Bye world output

--- GroupProject/src/main/java/world/perplex/groupproject/MainClass.java
+++ GroupProject/src/main/java/world/perplex/groupproject/MainClass.java
@@ -12,5 +12,6 @@ package world.perplex.groupproject;
 public class MainClass {
     public static void main(String[]args) {
         System.out.println("Hello World!");
+        System.out.println("Bye World!");
     }
 }
